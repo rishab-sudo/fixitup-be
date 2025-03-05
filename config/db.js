3d2e41acb1cd57ac6ecd78311ebe47350927798f
rishab-sudo <rishabdutt4@gmail.com>
2025-03-05 16:44:42 +0530

    awslambda-test

--- config/db.js
+++ config/db.js
@@ -1,37 +1,19 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://contactfixitupinfo:Bk6v2ZxzSyUpcGjR@cluster0.rlluj.mongodb.net/",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connected successfully!");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
-    process.exit(1);
+    process.exit(1); // Exit process with failure
   }
 };
 
 module.exports = connectDB;
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(
-//       "mongodb+srv://contactfixitupinfo:Bk6v2ZxzSyUpcGjR@cluster0.rlluj.mongodb.net/",
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     );
-//     console.log("MongoDB connected successfully!");
-//   } catch (err) {
-//     console.error("MongoDB connection failed:", err.message);
-//     process.exit(1); // Exit process with failure
-//   }
-// };
-
-// module.exports = connectDB;
