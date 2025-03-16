4192e4df7554db370f107b6db788207433d52362
Prashant701790 <prashant.upadhyay@aqad.ae>
2025-03-16 22:45:19 +0530

    New Code

--- lambda.js
+++ lambda.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const serverless = require("serverless-http");
-const connectDB = require("./config/db");
-const orderRoutes = require("./routes/order");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const serverless = require("serverless-http");
+// const connectDB = require("./config/db");
+// const orderRoutes = require("./routes/order");
 
-require("dotenv").config();
-const app = express();
+// require("dotenv").config();
+// const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: [
-      "http://your-frontend-url.com",
-      "http://localhost:3000",
-      "https://main.d269i1a354ml7x.amplifyapp.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://your-frontend-url.com",
+//       "http://localhost:3000",
+//       "https://main.d269i1a354ml7x.amplifyapp.com",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
-// Routes
-app.use("/api/orders", orderRoutes);
+// // Routes
+// app.use("/api/orders", orderRoutes);
 
-// Connect MongoDB
-connectDB();
+// // Connect MongoDB
+// connectDB();
 
-// Export as Lambda function
-module.exports.handler = serverless(app);
+// // Export as Lambda function
+// module.exports.handler = serverless(app);
